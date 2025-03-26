6db6501cca1a01abf4ff57f2ba1bca4d24d9a635
Keaton Palmer <me@keatonpalmer.com>
2025-03-26 18:20:49 -0400

    Add Day 4 task.py files

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,13 @@
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+import random
+
+
+# Option 1
+roulette = random.choice(friends)
+print(roulette)
+
+# Can also be simplified to print(random.choice(friends))
+
+#Option 2
+random_index = random.randint(0, 4)
+print(friends[random_index])
---
+++ Day 4/Lists/task.py
@@ -0,0 +1,14 @@
+# Lists
+fruits = ["Cherry", "Apple", "Pear"]
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+# You can edit an item in a list by selecting it's position
+states_of_america[1] = "Pencilvania"
+
+# Appends the list with what is in the parenthesis
+states_of_america.append("Mika Bay")
+
+states_of_america.extend(["KeatonLand", "Shady Lane"])
+
+print(states_of_america)
